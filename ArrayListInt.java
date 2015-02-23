317d6c03d38c3f854dcb9d6580b01bc2d83e793a
BuildTools <unconfigured@null.spigotmc.org>
2015-02-23 16:51:41 +0100

    Método para añadir elementos a la lista creado

--- ArrayListInt.java
+++ ArrayListInt.java
@@ -1,16 +1,32 @@
-
 /**
- * 
+ * Clase ArrayListInt basada en un ArrayList.
  */
 public class ArrayListInt
 {
-
+    // Lista de enteros.
+    private int[] lista;
     /**
-     * Constructor for objects of class ArrayListInt
+     * Constructor de la clase ArrayListInt
      */
     public ArrayListInt()
     {
-       
+       lista = new int [0]; // Se inicializa con 0 elementos para ahorrar espacio.
     }
-
+    
+    /**
+     * Añade un elemento al final de la lista.
+     * @param elemento El elemento a añadir.
+     */
+    public void add(int elemento)
+    {
+        int size = lista.length;
+        int[] temp = new int [size + 1]; // Se crea una lista temporal con el tamaño de la lista + 1.        
+        for(int i = 0; i < size; i++)
+        {
+            temp[i] = lista[i]; // Se guardan todos los elementos de la primer lista en la temporal.
+        }
+        temp [size] = elemento;
+        lista = temp;
+    }
+    
 }
