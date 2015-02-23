d6f7a31f20d2eb7712be7f9c40c931197af5a700
BuildTools <unconfigured@null.spigotmc.org>
2015-02-23 17:11:24 +0100

    Método para añadir un elemento en el indice especificado creado

--- ArrayListInt.java
+++ ArrayListInt.java
@@ -10,9 +10,9 @@ public class ArrayListInt
      */
     public ArrayListInt()
     {
-       lista = new int [0]; // Se inicializa con 0 elementos para ahorrar espacio.
+        lista = new int [0]; // Se inicializa con 0 elementos para ahorrar espacio.
     }
-    
+
     /**
      * Añade un elemento al final de la lista.
      * @param elemento El elemento a añadir.
@@ -28,5 +28,34 @@ public class ArrayListInt
         temp [size] = elemento;
         lista = temp;
     }
+
+    /**
+     * Añade un elemento en el indice especificado.
+     * @param index El indice donde añadir el elemento.
+     * @param elemento El elemento a añadir.
+     */
+    public void add(int index, int elemento)
+    {
+        int size = lista.length;
+        int[] temp = new int [size + 1]; // Se crea una lista temporal con el tamaño de la lista + 1. 
+        int i = 0;
+        if ((index <= size) && (index >= 0))
+        {
+            for(; i < index; i++)
+            {
+                temp [i] = lista [i]; // Se guardan todos los elementos de la primer lista en la temporal.
+            }
+            temp[index] = elemento;
+            for(int cont = i; cont < size; cont++)
+            {
+                temp[cont + 1] = lista[cont];
+            }
+            lista = temp;
+        }
+        else
+        {
+            System.out.println("Introduce un índice que esté entre 0 y " + size);
+        }
+    }    
     
 }
